0c6ab2da3575db99ef3953118e5a2adb10185f8a
Yuvashreek26 <yuvashree903@gmail.com>
2025-08-31 23:05:59 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def add(x y):
+  return x + y
+def sub(x y):
+  return x - y
+def multiply(x y):
+  return (x*y)
+def divide(x y):
+  if y==o :
+          return "undefined"
+  else :
+          return (x/y)
+print ("addition" , add(5,3))
+print ("subtraction",sub(5,3))
+print("multiplication", multiply(5,3))
+print("division", divide(5,3))
